9686685b9b34bb029d28a05974312a16509842ce
WillBennett03 <84158122+WillBennett03@users.noreply.github.com>
2022-04-24 16:31:19 +0100

    Update database.py
    removed private url

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+url = ''
 
-client = MongoClient("mongodb+srv://WBennett03:mK96Dco67cCbZitx@cluster0.j4bhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient(url)
 db = client.myFirstDatabase
 
 db.record.insert_one({'TEST' : {'sect' : 'meme', 'data' : [[1,1],[1,2],[1,3]]}})
